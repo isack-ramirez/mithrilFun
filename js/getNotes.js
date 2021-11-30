f77e6af3cfae5a1a07397dd59ee91fbdc8737487
Isack Ramirez <isack.ramirez1@gmail.com>
2021-11-29 19:52:15 -0600

    kind of sloppy but im trying to get an idea of the best way to use this framework, implemented button textfield and a state for it

---
+++ js/getNotes.js
@@ -0,0 +1,15 @@
+
+init();
+
+
+function init(){
+    var values=[],
+        keys=Object.keys(localStorage),
+        i = keys.length;
+
+    while(i--){
+        values.push(localStorage.getItem(keys[i]))
+    }
+
+    return values;
+}
